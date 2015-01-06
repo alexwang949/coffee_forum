59074777ae4c2e87ae8b6cd267362dc5ca464b89
alexwang949 <alex_cellist@yahoo.com>
2015-01-06 10:24:02 -0500

    css

--- project_1/schema.sql
+++ project_1/schema.sql
@@ -14,7 +14,7 @@ INSERT INTO categories (title, votes, description) VALUES ('Latte Art', 9, 'crea
 INSERT INTO categories (title, votes, description) VALUES ('All Brew Methods', 30, 'experience with any brew method');
 
 INSERT INTO posts (user_name, entry, category, votes, post_title) VALUES ('alexwang38', 'which grind size do you use?', 'v60 methods', 8, 'v60 is the best brew method.');
-INSERT INTO posts (user_name, entry, category, votes, post_title) VALUES ('alexwang38', 'which grind size do you use?', 'v60 methods', 8, 'v60 is the best brew method.');
+INSERT INTO posts (user_name, entry, category, votes, post_title) VALUES ('alexwang38', 'which grind size do you use?', 'v60 methods', 10, 'v60 is the best brew method.');
 
 
 
